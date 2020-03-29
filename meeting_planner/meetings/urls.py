be34b52f21b1b08ad9f244e6106c86a974ad9a00
Mariano Soto <marianosoto.88@live.com>
2020-03-29 12:22:36 -0300

    Add template to create a new meeting

--- meeting_planner/meetings/urls.py
+++ meeting_planner/meetings/urls.py
@@ -3,9 +3,10 @@
 #      CONST_NAME_RGX value
 
 from django.urls import path
-from meetings.views import meeting_detail, room_list
+from meetings.views import meeting_detail, new_meeting, room_list
 
 urlpatterns = [
     path('<int:id>', meeting_detail, name='meeting_detail'),
     path('rooms', room_list, name='room_list'),
+    path('new', new_meeting, name='new_meeting'),
 ]
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.forms import modelform_factory
+from django.shortcuts import get_object_or_404, render
 from meetings.models import Meeting, Room
 
 
@@ -6,6 +7,15 @@ def meeting_detail(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/meeting_detail.html", {"meeting": meeting})
 
+
 def room_list(request):
     rooms = Room.objects.all()
     return render(request, "meetings/room_list.html", {"rooms": rooms})
+
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+
+def new_meeting(request):
+    form = MeetingForm()
+    return render(request, "meetings/new_meeting.html", {"form": form})
